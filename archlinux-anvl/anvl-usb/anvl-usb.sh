29196cb6b96c228ecc7c58d7179d310bfe599f51
networkimprov <github@networkimprov.net>
2014-09-19 10:46:31 -0700

    in anvl-usb: add anvl-getty-usb@.service, anvl-getty.service; PKGBUILD not updated

--- archlinux-anvl/anvl-usb/anvl-usb.sh
+++ archlinux-anvl/anvl-usb/anvl-usb.sh
@@ -18,59 +18,58 @@ usb_controller="musb-hdrc.0.auto"
 # https://wiki.tizen.org/wiki/USB/Linux_USB_Layers/Configfs_Composite_Gadget/Usage_eq._to_g_multi.ko
 #
 start() {
-        modprobe libcomposite > /dev/null 2>&1
-        mount -t configfs none /sys/kernel/config > /dev/null 2>&1
+	modprobe libcomposite > /dev/null 2>&1
+	mount -t configfs none /sys/kernel/config > /dev/null 2>&1
 
 	if [ ! -d /sys/kernel/config ]; then
 		echo "ERROR: No configfs found"
 		return 1
 	fi
 
-        mkdir /sys/kernel/config/usb_gadget/g1
-        old_pwd=$(pwd)
-        cd /sys/kernel/config/usb_gadget/g1
-        
-        echo $product > idProduct
-        echo $vendor > idVendor
-        mkdir strings/0x409
-        echo $omap_dieid > strings/0x409/serialnumber
-        echo $manufacturer > strings/0x409/manufacturer
-        echo "Multi Gadget" > strings/0x409/product
-        
-        mkdir configs/c.1
-        echo 120 > configs/c.1/MaxPower
-        mkdir configs/c.1/strings/0x409
-        echo "Conf 1" > configs/c.1/strings/0x409/configuration
-        
-        mkdir configs/c.2
-        echo 500 > configs/c.2/MaxPower
-        mkdir configs/c.2/strings/0x409
-        echo "Conf 2" > configs/c.2/strings/0x409/configuration
-        
-        mkdir functions/mass_storage.0
-        echo $file0 > functions/mass_storage.0/lun.0/file
+	mkdir /sys/kernel/config/usb_gadget/g1
+	old_pwd=$(pwd)
+	cd /sys/kernel/config/usb_gadget/g1
+	
+	echo $product > idProduct
+	echo $vendor > idVendor
+	mkdir strings/0x409
+	echo $omap_dieid > strings/0x409/serialnumber
+	echo $manufacturer > strings/0x409/manufacturer
+	echo "Multi Gadget" > strings/0x409/product
+	
+	mkdir configs/c.1
+	echo 120 > configs/c.1/MaxPower
+	mkdir configs/c.1/strings/0x409
+	echo "Conf 1" > configs/c.1/strings/0x409/configuration
+	
+	mkdir configs/c.2
+	echo 500 > configs/c.2/MaxPower
+	mkdir configs/c.2/strings/0x409
+	echo "Conf 2" > configs/c.2/strings/0x409/configuration
+	
+	mkdir functions/mass_storage.0
+	echo $file0 > functions/mass_storage.0/lun.0/file
   
-        mkdir functions/acm.0
-        mkdir functions/ecm.0
-        mkdir functions/rndis.0
-        
-        ln -s functions/rndis.0 configs/c.1
-        ln -s functions/acm.0 configs/c.1
-        ln -s functions/mass_storage.0 configs/c.1
-        
-        ln -s functions/ecm.0 configs/c.2
-        ln -s functions/acm.0 configs/c.2
-        ln -s functions/mass_storage.0 configs/c.2
-        
-        echo $usb_controller > /sys/kernel/config/usb_gadget/g1/UDC
-        cd $old_pwd
+	mkdir functions/acm.0
+	mkdir functions/ecm.0
+	mkdir functions/rndis.0
+	
+	ln -s functions/rndis.0 configs/c.1
+	ln -s functions/acm.0 configs/c.1
+	ln -s functions/mass_storage.0 configs/c.1
+	
+	ln -s functions/ecm.0 configs/c.2
+	ln -s functions/acm.0 configs/c.2
+	ln -s functions/mass_storage.0 configs/c.2
+	
+	echo $usb_controller > /sys/kernel/config/usb_gadget/g1/UDC
+	cd $old_pwd
 
 	/usr/bin/usb-manager $(find /sys -name vbus | grep twl4030-usb) &
 }
 
 stop() {
 	killall usb-manager > /dev/null 2>&1
-	systemctl stop getty@ttyGS0.service
 	if [ ! -d /sys/kernel/config ]; then
 		echo "ERROR: No configfs found"
 		return 1
@@ -87,3 +86,4 @@ case $1 in
 		exit 1
 	;;
 esac
+
